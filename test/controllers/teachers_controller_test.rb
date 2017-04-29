67d6c47e57a7d171b8b8e52f9e00ece0ec23e031
Kevin Morgan <kevismorgis@gmail.com>
2017-04-29 19:25:24 +0000

    Added validation to teachers.

--- test/controllers/teachers_controller_test.rb
+++ test/controllers/teachers_controller_test.rb
@@ -5,21 +5,10 @@ class TeachersControllerTest < ActionDispatch::IntegrationTest
     @teacher = teachers(:one)
   end
 
-  test "should get index" do
-    get teachers_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_teacher_url
-    assert_response :success
-  end
-
   test "should create teacher" do
     assert_difference('Teacher.count') do
-      post teachers_url, params: { teacher: { admin_powers: @teacher.admin_powers, analysis_powers: @teacher.analysis_powers, color: @teacher.color, school_id: @teacher.school_id, teacher_description: @teacher.teacher_description, teacher_email: @teacher.teacher_email, teacher_icon_name: @teacher.teacher_icon_name, teacher_name: @teacher.teacher_name, user_name: @teacher.user_name } }
+      post teachers_url, params: { teacher: { admin_powers: @teacher.admin_powers, analysis_powers: @teacher.analysis_powers, color: @teacher.color, school_id: @teacher.school_id, teacher_description: @teacher.teacher_description, teacher_email: "email@example.com", teacher_icon_name: @teacher.teacher_icon_name, teacher_name: @teacher.teacher_name, user_name: @teacher.user_name } }
     end
-
     assert_redirected_to teacher_url(Teacher.last)
   end
 
@@ -27,22 +16,10 @@ class TeachersControllerTest < ActionDispatch::IntegrationTest
     get teacher_url(@teacher)
     assert_response :success
   end
-
-  test "should get edit" do
-    get edit_teacher_url(@teacher)
-    assert_response :success
-  end
-
-  test "should update teacher" do
-    patch teacher_url(@teacher), params: { teacher: { admin_powers: @teacher.admin_powers, analysis_powers: @teacher.analysis_powers, color: @teacher.color, school_id: @teacher.school_id, teacher_description: @teacher.teacher_description, teacher_email: @teacher.teacher_email, teacher_icon_name: @teacher.teacher_icon_name, teacher_name: @teacher.teacher_name, user_name: @teacher.user_name } }
-    assert_redirected_to teacher_url(@teacher)
-  end
-
-  test "should destroy teacher" do
-    assert_difference('Teacher.count', -1) do
-      delete teacher_url(@teacher)
+  
+  test "should reject invalid email" do
+    assert_no_difference('Teacher.count') do
+      post teachers_url, params: { teacher: { admin_powers: @teacher.admin_powers, analysis_powers: @teacher.analysis_powers, color: @teacher.color, school_id: @teacher.school_id, teacher_description: @teacher.teacher_description, teacher_email: @teacher.teacher_email, teacher_icon_name: @teacher.teacher_icon_name, teacher_name: @teacher.teacher_name, user_name: @teacher.user_name } }
     end
-
-    assert_redirected_to teachers_url
   end
 end
