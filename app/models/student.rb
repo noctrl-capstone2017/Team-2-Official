8d93083038310437bebcc946660dfc217bbb887c
Ricky Perez <ecperez@noctrl.edu>
2017-05-08 14:07:20 +0000

    fix merge conflicts

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
     validates :student_name, presence: true
-    validates :student_icon_name, presence: true
+    validates :student_icon_name, presence: true, uniqueness: true
     validates :student_description, presence: true
     validates :color, presence: true
     validates :student_icon, presence: true
