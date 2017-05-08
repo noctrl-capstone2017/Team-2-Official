61a14485c92a4d44dc7d0acf7725c6d978c07b43
Ricky Perez <ecperez@noctrl.edu>
2017-05-08 14:08:59 +0000

    Fix merge conflicts

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,13 @@
 class Student < ApplicationRecord
-    #Ensures values can't be empty except for desc. icon name must be unique.
     validates :student_name, presence: true
     validates :student_icon_name, presence: true, uniqueness: true
-    validates :student_contact_info, presence: true
+    validates :student_description, presence: true
     validates :color, presence: true
     validates :student_icon, presence: true
+    
+   #Creates the relationship of what teachers the student belongs too
+    has_many :passive_relationships, class_name:  "RosterStudent",
+                                     foreign_key: "student_id",
+                                     dependent:   :destroy
+    has_many :teachers, through: :passive_relationships, source: :teacher
 end
