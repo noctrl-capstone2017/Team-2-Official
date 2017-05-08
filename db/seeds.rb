888b9286bd82fe28ae7545b049068ce32a0fcfa7
alexpavia <your.aapavia@noctrl.edu>
2017-05-08 17:46:14 +0000

    Fixed student squares to add the students icon/color on teachers home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,11 +32,11 @@ end
 
 10.times do |n|
   name  = Faker::Name.name
-  Student.create!(student_icon_name: "test",
+  Student.create!(student_icon_name: "temp",
                     student_contact_info: "student contact info",
                     student_description: "student description",
-                    student_icon: "temp",
-                    color: "red",
+                    student_icon: "fa fa-bicycle fa-5x student-icon",
+                    color: "green",
                     school_id: 1,
                     student_name: name)
 end
